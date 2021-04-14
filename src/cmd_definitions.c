d8c7c8bb26af624d5341aab60cd4d6622474bf8d
Jan200101 <sentrycraft123@gmail.com>
2021-04-14 22:35:26 +0200

    add help message

--- src/cmd_definitions.c
+++ src/cmd_definitions.c
@@ -8,10 +8,12 @@
 #define COUNT_OF(arr) (sizeof(arr) / sizeof(arr[0]))
 
 const cmd_definition cmd_definitions[] = {
-    {.name = "destroy", .arg_count = 0, .executor = &cmd_destroy},
-    {.name = "launch", .arg_count = 1, .executor = &cmd_launch},
-    {.name = "reboot", .arg_count = 0, .executor = &cmd_reboot},
-    {.name = "screen", .arg_count = 1, .executor = &cmd_screen}};
+    {.name = "help", .description = "Display this help screen", .arg_count = 0, .executor = &cmd_help},
+    {.name = "destroy", .description = "Kill all running applications", .arg_count = 0, .executor = &cmd_destroy},
+    {.name = "launch", .description = "Launch an app by Title ID", .arg_count = 1, .executor = &cmd_launch},
+    {.name = "reboot", .description = "Reboot the console", .arg_count = 0, .executor = &cmd_reboot},
+    {.name = "screen", .description = "Turn the screen on or off", .arg_count = 1, .executor = &cmd_screen}
+};
 
 const cmd_definition *cmd_get_definition(char *cmd_name) {
   for (unsigned int i = 0; i < COUNT_OF(cmd_definitions); i++) {
@@ -21,6 +23,28 @@ const cmd_definition *cmd_get_definition(char *cmd_name) {
   }
 
   return NULL;
+}
+
+void cmd_help(char **arg_list, size_t arg_count, char *res_msg) {
+  char buf[2000] = {0};
+  int longest_cmd = 0;
+
+  for (int i = 0; i < COUNT_OF(cmd_definitions); ++i) {
+    int cmd_length = strlen(cmd_definitions[i].name);
+
+    if (cmd_length > longest_cmd) {
+      longest_cmd = cmd_length;
+    }
+  }
+
+  sprintf(buf, "%-*s\t\t%s\n", longest_cmd, "Command", "Description");
+  strcpy(res_msg, buf);
+
+  for (int i = 0; i < COUNT_OF(cmd_definitions); ++i) {
+    sprintf(buf, "%-*s\t\t%s\n", longest_cmd, cmd_definitions[i].name, cmd_definitions[i].description);
+    strcat(res_msg, buf);
+  }
+
 }
 
 void cmd_destroy(char **arg_list, size_t arg_count, char *res_msg) {
--- src/cmd_definitions.h
+++ src/cmd_definitions.h
@@ -6,11 +6,13 @@ typedef void cmd_executor(char **arg_list, size_t arg_count, char *res_msg);
 
 typedef struct {
     char  *name;
+    char  *description;
     size_t arg_count;
     cmd_executor *executor;
 } cmd_definition;
 
 const cmd_definition *cmd_get_definition(char *cmd_name);
+void cmd_help(char **arg_list, size_t arg_count, char *res_msg);
 void cmd_destroy(char **arg_list, size_t arg_count, char *res_msg);
 void cmd_launch(char **arg_list, size_t arg_count, char *res_msg);
 void cmd_reboot(char **arg_list, size_t arg_count, char *res_msg);
